2cb1c61ba79d37d1e4bc93109cd286667f349284
Ved Prakash <vedprakash182001@gmail.com>
2025-06-27 22:21:34 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <div style={{ fontSize: '24px' }}>
+        Corporate Sathi coming soon!!....................
+      </div>
+    </div>
+  );
+}
+
+export default App;
